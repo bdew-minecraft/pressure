e33476c75748124b52e1ddee12e3a0807269810d
bdew <blackdew@gmail.com>
2014-06-09 14:11:36 +0300

    Added generation of API jar, removed deps subproject (as we don't have any \o/)

---
+++ src/net/bdew/pressure/api/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) bdew, 2013 - 2014
+ * https://github.com/bdew/pressure
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * https://raw.github.com/bdew/pressure/master/MMPL-1.0.txt
+ */
+
+@API(apiVersion = "PRESSURE_APIVER", owner = "pressure", provides = "pressureAPI") //
+        package net.bdew.pressure.api;
+
+import cpw.mods.fml.common.API;
