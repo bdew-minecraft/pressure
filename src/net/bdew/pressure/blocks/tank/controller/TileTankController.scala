a5c155dc31b3079b72377e1fc015df90e17cadd1
bdew <blackdew@gmail.com>
2014-09-16 13:12:27 +0300

    Added container filling/draining to tank GUI

--- src/net/bdew/pressure/blocks/tank/controller/TileTankController.scala
+++ src/net/bdew/pressure/blocks/tank/controller/TileTankController.scala
@@ -9,7 +9,9 @@
 
 package net.bdew.pressure.blocks.tank.controller
 
+import net.bdew.lib.Misc
 import net.bdew.lib.data.{DataSlotInventory, DataSlotTank}
+import net.bdew.lib.items.ItemUtils
 import net.bdew.lib.multiblock.interact.{CIFluidInput, CIFluidOutput, CIOutputFaces}
 import net.bdew.lib.multiblock.tile.TileControllerGui
 import net.bdew.pressure.blocks.tank.MachineTank
@@ -33,7 +35,94 @@ class TileTankController extends TileControllerGui with CIFluidInput with CIOutp
 
   lazy val maxOutputs = 6
 
+  def canEjectItem(stack: ItemStack) = {
+    val outstack = inventory.getStackInSlot(1)
+    (stack == null
+      || stack.getItem == null
+      || outstack == null
+      || outstack.getItem == null
+      || (ItemUtils.isSameItem(stack, outstack) && outstack.stackSize + stack.stackSize <= outstack.getMaxStackSize)
+      )
+  }
+
+  def doEjectItem(stack: ItemStack) {
+    if (stack != null && stack.getItem != null) {
+      stack.stackSize = 1
+      if (inventory.getStackInSlot(1) == null) {
+        inventory.setInventorySlotContents(1, stack)
+      } else {
+        inventory.getStackInSlot(1).stackSize += stack.stackSize
+        inventory.markDirty()
+      }
+    }
+  }
+
   def doUpdate() {
+    val instack = inventory.getStackInSlot(0)
+    if (instack != null && instack.getItem != null) {
+      if (FluidContainerRegistry.isFilledContainer(instack)) {
+        // This is a full container, add fluid into tank and return empty container (if defined)
+        val fluid = FluidContainerRegistry.getFluidForFilledItem(instack)
+        val cont = instack.getItem.getContainerItem(instack)
+        if (fluid != null && inputFluid(fluid, false) == fluid.amount && canEjectItem(cont)) {
+          inputFluid(fluid, true)
+          inventory.decrStackSize(0, 1)
+          doEjectItem(cont)
+          inventory.markDirty()
+        }
+      } else if (FluidContainerRegistry.isEmptyContainer(instack)) {
+        // This is an empty container, fill it
+        for {
+          filled <- Option(FluidContainerRegistry.fillFluidContainer(tank.getFluid, instack)) if canEjectItem(filled)
+          filledFluid <- Option(FluidContainerRegistry.getFluidForFilledItem(filled))
+        } {
+          tank.drain(filledFluid.amount, true)
+          inventory.decrStackSize(0, 1)
+          doEjectItem(filled)
+        }
+      } else Misc.asInstanceOpt(instack.getItem, classOf[IFluidContainerItem]) map { incont =>
+        // Its a fluid container, figure out what to do with it
+        // Operating on a stack makes no sense, grab a single item
+        val remstack = instack.copy()
+        val opstack = remstack.splitStack(1)
+        val influid = incont.getFluid(opstack)
+        if (influid != null && influid.amount > 0) {
+          // It has something, try to drain it
+          val filled = incont.drain(opstack, tank.fill(influid.copy(), false), false)
+          if (filled != null && filled.amount > 0) {
+            // Can drain, grab a drained version and see if we can output it
+            incont.drain(opstack, filled.amount, true)
+            if (canEjectItem(opstack)) {
+              // All good, proceed
+              tank.fill(filled, true)
+              doEjectItem(opstack)
+              // put the remaining stack (if any) back
+              if (remstack.stackSize > 0)
+                inventory.setInventorySlotContents(0, remstack)
+              else
+                inventory.setInventorySlotContents(0, null)
+            }
+          }
+        } else if (tank.getFluid != null) {
+          // It's empty and we have fluid, try to fill it
+          val filled = tank.drain(incont.fill(opstack, tank.getFluid.copy(), false), false)
+          if (filled != null && filled.amount > 0) {
+            // Can fill, grab a filled version and see if we can output it
+            incont.fill(opstack, filled, true)
+            if (canEjectItem(opstack)) {
+              // All good, proceed
+              tank.drain(filled.amount, true)
+              doEjectItem(opstack)
+              // put the remaining stack (if any) back
+              if (remstack.stackSize > 0)
+                inventory.setInventorySlotContents(0, remstack)
+              else
+                inventory.setInventorySlotContents(0, null)
+            }
+          }
+        }
+      }
+    }
   }
 
   serverTick.listen(doUpdate)
