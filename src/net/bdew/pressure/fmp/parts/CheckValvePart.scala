2eb38ac5740f9c205f6300968ad551438b940435
bdew <blackdew@gmail.com>
2015-03-16 10:50:38 +0200

    Fixed FMP check valve getting RS signal through full covers

--- src/net/bdew/pressure/fmp/parts/CheckValvePart.scala
+++ src/net/bdew/pressure/fmp/parts/CheckValvePart.scala
@@ -9,6 +9,7 @@
 
 package net.bdew.pressure.fmp.parts
 
+import codechicken.multipart.{IRedstonePart, RedstoneInteractions, TMultiPart}
 import net.bdew.lib.Misc
 import net.bdew.lib.data.base.UpdateKind
 import net.bdew.lib.data.{DataSlotBoolean, DataSlotDirection}
@@ -18,7 +19,7 @@ import net.bdew.pressure.pressurenet.Helper
 import net.minecraftforge.common.util.ForgeDirection
 import net.minecraftforge.fluids.FluidStack
 
-class CheckValvePart(aFacing: ForgeDirection = BlockCheckValve.getDefaultFacing, aIsPowered: Boolean = false) extends BaseValvePart(BlockCheckValve, "bdew.pressure.checkvalve") {
+class CheckValvePart(aFacing: ForgeDirection = BlockCheckValve.getDefaultFacing, aIsPowered: Boolean = false) extends BaseValvePart(BlockCheckValve, "bdew.pressure.checkvalve") with IRedstonePart {
   def this(meta: Int) = this(Misc.forgeDirection(meta & 7), (meta & 8) == 8)
 
   override val facing = DataSlotDirection("facing", this).setUpdate(UpdateKind.WORLD, UpdateKind.SAVE)
@@ -34,8 +35,18 @@ class CheckValvePart(aFacing: ForgeDirection = BlockCheckValve.getDefaultFacing,
     } else 0
   }
 
+  override def strongPowerLevel(side: Int) = 0
+  override def weakPowerLevel(side: Int) = 0
+  override def canConnectRedstone(side: Int) = side != facing.ordinal() && side != facing.getOpposite.ordinal()
+
+  override def onPartChanged(part: TMultiPart) = {
+    super.onPartChanged(part)
+    onNeighborChanged()
+  }
+
   override def onNeighborChanged(): Unit = {
-    val powered = world.isBlockIndirectlyGettingPowered(x, y, z)
+    val sides = ForgeDirection.VALID_DIRECTIONS.toSet -- Set(facing.value, facing.value.getOpposite)
+    val powered = sides.exists(s => RedstoneInteractions.getPowerTo(this, s.ordinal()) > 0)
     if (powered != isPowered.value) {
       isPowered := powered
       tile.notifyPartChange(this)
