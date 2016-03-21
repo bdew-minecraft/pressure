7d8a1fc9da7853b2faf232ea36ae8e32565eaa3c
bdew <blackdew@gmail.com>
2016-03-21 22:14:20 +0200

    Restored mcmp stuff, updated it to 1.9

---
+++ src/net/bdew/pressure/mutilpart/MCMPUtils.scala
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) bdew, 2013 - 2016
+ * https://github.com/bdew/pressure
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://bdew.net/minecraft-mod-public-license/
+ */
+
+package net.bdew.pressure.mutilpart
+
+import mcmultipart.multipart.{IMultipart, IMultipartContainer, MultipartHelper}
+import net.bdew.lib.Misc
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.IBlockAccess
+
+import scala.collection.JavaConversions._
+
+object MCMPUtils {
+  def getTypedParts[T <: IMultipart](cls: Class[T], te: IMultipartContainer): Iterable[T] =
+    Misc.filterType(te.getParts, cls)
+
+  def getTypedParts[T <: IMultipart](cls: Class[T], w: IBlockAccess, p: BlockPos): Iterable[T] =
+    Option(MultipartHelper.getPartContainer(w, p)) map (te => Misc.filterType(te.getParts, cls)) getOrElse List.empty
+}
